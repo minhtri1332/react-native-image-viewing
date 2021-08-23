6b90b46f73b56ceae512e1e9566491e913ed6d77
minhtri1332 <53599100+minhtri1332@users.noreply.github.com>
2021-08-23 15:46:53 +0700

    Update ImageItem.ios.tsx
    update item ios

--- src/components/ImageItem/ImageItem.ios.tsx
+++ src/components/ImageItem/ImageItem.ios.tsx
@@ -36,6 +36,7 @@ const SCREEN_HEIGHT = SCREEN.height;
 type Props = {
   imageSrc: ImageSource;
   onRequestClose: () => void;
+  onOffsetY: (offsetY: number) => void;
   onZoom: (scaled: boolean) => void;
   onLongPress: (image: ImageSource) => void;
   delayLongPress: number;
@@ -46,6 +47,7 @@ type Props = {
 const ImageItem = ({
   imageSrc,
   onZoom,
+  onOffsetY,
   onRequestClose,
   onLongPress,
   delayLongPress,
@@ -59,21 +61,16 @@ const ImageItem = ({
   const handleDoubleTap = useDoubleTapToZoom(scrollViewRef, scaled, SCREEN);
 
   const [translate, scale] = getImageTransform(imageDimensions, SCREEN);
-  const scrollValueY = new Animated.Value(0);
   const scaleValue = new Animated.Value(scale || 1);
   const translateValue = new Animated.ValueXY(translate);
   const maxScale = scale && scale > 0 ? Math.max(1 / scale, 1) : 1;
 
-  const imageOpacity = scrollValueY.interpolate({
-    inputRange: [-SWIPE_CLOSE_OFFSET, 0, SWIPE_CLOSE_OFFSET],
-    outputRange: [0.5, 1, 0.5],
-  });
   const imagesStyles = getImageStyles(
     imageDimensions,
     translateValue,
     scaleValue
   );
-  const imageStylesWithOpacity = { ...imagesStyles, opacity: imageOpacity };
+  const imageStylesWithOpacity = { ...imagesStyles };
 
   const onScrollEndDrag = useCallback(
     ({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
@@ -103,7 +100,7 @@ const ImageItem = ({
       return;
     }
 
-    scrollValueY.setValue(offsetY);
+    onOffsetY(offsetY);
   };
 
   const onLongPressHandler = useCallback(
